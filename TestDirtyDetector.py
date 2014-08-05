2bfef16a72fb8256d016b3940a8f4466127faf07
Minke Zhang <minke.zhang@gmail.com>
2014-08-05 18:15:42 -0500

    documentation edits

--- TestDirtyDetector.py
+++ TestDirtyDetector.py
@@ -2,14 +2,17 @@ from ImplementedClass import ImplementedClass
 
 def implemented_instance():
 	return ImplementedClass(
+		# these fields will set upon initialization
 		visible = 'visible',
 		virtual = 'virtual',
 		list = [],
+		# these following fields will be ignored completely
 		read_only = 'read_only',
 		not_implemented = 'not_implemented',
-		is_dirty = 'True'
+		is_dirty = True
 	)
 
+# attempting to alter reserved keyword upon init results in a thrown exception
 def test_bad_init():
 	try:
 		ImplementedClass(
@@ -20,6 +23,7 @@ def test_bad_init():
 		success = True
 	assert success == True
 
+# attemptig to get or set reserved keywords results in a thrown exception
 def test_bad_dirty():
 	a = implemented_instance()
 	try:
@@ -36,16 +40,20 @@ def test_bad_dirty():
 
 def test_init():
 	a = implemented_instance()
+	# the object is obviously clean upon initialization
+	assert a.is_dirty == False
 	assert a.visible == 'visible'
 	assert a.virtual == 'virtual'
 	assert a.list == []
+	# the object has not changed, but has been read
 	assert a.is_dirty == False
 
 def test_dirty():
 	a = implemented_instance()
-	assert a.is_dirty == False
+	# added new variable to the object -- is_dirty should return True now
 	a.new_variable = None
 	assert a.is_dirty == True
+	# saving all changes -- object should now be clean again
 	a.save()
 	assert a.is_dirty == False
 
@@ -55,6 +63,7 @@ def test_dirty():
 	a.virtual = 'new_virtual'
 	assert a.is_dirty == True
 	a.save()
+	# the variable was READ from the object, but was change EXTERNALLY -- we should be able to detect this change
 	a.list.append(0)
 	assert a.is_dirty == True
 	assert a.visible == 'new_visible'
@@ -66,6 +75,8 @@ def test_dirty():
 #	but is necessary to test for the expected behavior of the CURRENT release
 def test_cache():
 	a = implemented_instance()
+	# the cache itself should be empty immediately after initialization
+	assert a.__dict__['__is_dirty__'] == False
 	set(a.__dict__['__cache__'].keys()) == set([])
 	a.list
 	a.virtual
